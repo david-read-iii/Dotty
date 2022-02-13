274a3c2f5289375ecaf3a0fb574e7c2c822568b7
David Read <49120229+david-read-iii@users.noreply.github.com>
2022-02-12 19:16:51 -0500

    Setup controller MainActivity class
    MainActivity.java:
    - Define class member variables as mGame to model game state, mDotsGrid to display the game board, mMovesRemaining to display how many moves are remaining, and mScore to display the game score.
    - Define onCreate() to initialize member variables and initialize the user interface.
    - Define mGridListener to specify what to do when a dot is clicked in mDotsGrid.
    - Define newGameClick() hook to call startNewGame().
    - Define startNewGame() to reset mGame and update the UI to match the reset.
    - Define updateMovesAndScore() to update the moves and score counters.

--- app/src/main/java/com/davidread/dotty/MainActivity.java
+++ app/src/main/java/com/davidread/dotty/MainActivity.java
@@ -3,12 +3,109 @@ package com.davidread.dotty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import java.util.Locale;
+
+/**
+ * {@link MainActivity} represents a user interface with a move counter, a score counter, a Dotty
+ * game board, and a new game button.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * {@link DotsGame} holding the logic and state of this Dotty game.
+     */
+    private DotsGame mGame;
+
+    /**
+     * {@link DotsGrid} displaying the game board and path of selected dots for this Dotty game.
+     */
+    private DotsGrid mDotsGrid;
+
+    /**
+     * {@link TextView} indicating how many moves are remaining.
+     */
+    private TextView mMovesRemaining;
+
+    /**
+     * {@link TextView} indicating the game score.
+     */
+    private TextView mScore;
+
+    /**
+     * Invoked once when this {@link MainActivity} is initially created. It initializes this
+     * activity's member variables and initializes the user interface.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMovesRemaining = findViewById(R.id.movesRemaining);
+        mScore = findViewById(R.id.score);
+        mDotsGrid = findViewById(R.id.gameGrid);
+        mDotsGrid.setGridListener(mGridListener);
+
+        mGame = DotsGame.getInstance();
+        startNewGame();
+    }
+
+    /**
+     * {@link DotsGrid.DotsGridListener} that specifies what to do when a dot is touched on
+     * {@link #mDotsGrid}. It registers each dot click from {@link #mDotsGrid} to {@link #mGame}.
+     */
+    private final DotsGrid.DotsGridListener mGridListener = new DotsGrid.DotsGridListener() {
+
+        @Override
+        public void onDotSelected(Dot dot, DotsGrid.DotSelectionStatus selectionStatus) {
+
+            // Ignore selections when game is over.
+            if (mGame.isGameOver()) return;
+
+            // Add/remove dot to/from selected dots.
+            DotsGame.DotStatus addStatus = mGame.processDot(dot);
+
+            // If done selecting dots then replace selected dots and display new moves and score.
+            if (selectionStatus == DotsGrid.DotSelectionStatus.Last) {
+                if (mGame.getSelectedDots().size() > 1) {
+                    mGame.finishMove();
+                    updateMovesAndScore();
+                } else {
+                    mGame.clearSelectedDots();
+                }
+            }
+
+            // Display changes to the game.
+            mDotsGrid.invalidate();
+        }
+    };
+
+    /**
+     * Invoked when the new game {@link android.widget.Button} is clicked in {@link MainActivity}.
+     * It simply calls {@link #startNewGame()}.
+     */
+    public void newGameClick(View view) {
+        startNewGame();
+    }
+
+    /**
+     * Resets the game state in {@link #mGame}, resets the UI of {@link #mDotsGrid}, and resets the
+     * UI of {@link #mMovesRemaining} and {@link #mScore}.
+     */
+    private void startNewGame() {
+        mGame.newGame();
+        mDotsGrid.invalidate();
+        updateMovesAndScore();
+    }
+
+    /**
+     * Updates {@link #mMovesRemaining} and {@link #mScore} with the latest game data from
+     * {@link #mGame}.
+     */
+    private void updateMovesAndScore() {
+        mMovesRemaining.setText(String.format(Locale.getDefault(), "%d", mGame.getMovesLeft()));
+        mScore.setText(String.format(Locale.getDefault(), "%d", mGame.getScore()));
     }
 }
